5bf0dc76f227c6030913ae8059d64db0b5d7e411
aslamcontact <aslam.mycontact@gmail.com>
2023-11-17 19:57:07 +0530

    add auth manager bean

--- src/main/java/com/aslam/mycontact/springtest/security/jwt/ApplicationConfig.java
+++ src/main/java/com/aslam/mycontact/springtest/security/jwt/ApplicationConfig.java
@@ -5,7 +5,9 @@ import com.mysql.cj.protocol.AuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,6 +36,11 @@ public class ApplicationConfig {
         authProvider.setPasswordEncoder(passwordEncoder());
         return (AuthenticationProvider) authProvider;
     }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+      return configuration.getAuthenticationManager();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder()
     {
