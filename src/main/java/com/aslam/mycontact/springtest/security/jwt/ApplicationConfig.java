b58f53b8cbbce969901aefc704e2f5ead0ed14dc
aslamcontact <aslam.mycontact@gmail.com>
2023-11-17 19:54:21 +0530

    add authenticationProvider and passwordEncoder

--- src/main/java/com/aslam/mycontact/springtest/security/jwt/ApplicationConfig.java
+++ src/main/java/com/aslam/mycontact/springtest/security/jwt/ApplicationConfig.java
@@ -1,11 +1,15 @@
 package com.aslam.mycontact.springtest.security.jwt;
 
 import com.aslam.mycontact.springtest.dao.AppUserRepository;
+import com.mysql.cj.protocol.AuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfig {
@@ -21,5 +25,20 @@ public class ApplicationConfig {
         )
                 ;
     }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider()
+    {
+        DaoAuthenticationProvider authProvider= new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return (AuthenticationProvider) authProvider;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
