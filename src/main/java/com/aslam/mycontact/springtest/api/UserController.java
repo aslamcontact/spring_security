ab78e840e71968f324d3d0d8c05da6d9bd3bf433
aslamcontact <aslam.mycontact@gmail.com>
2023-11-23 23:54:02 +0530

    add some changes

--- src/main/java/com/aslam/mycontact/springtest/api/UserController.java
+++ src/main/java/com/aslam/mycontact/springtest/api/UserController.java
@@ -1,12 +1,23 @@
 package com.aslam.mycontact.springtest.api;
 
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+
+
+
+    @GetMapping("/mes")
+    public ResponseEntity<String> sayHellow()
+    {
+        return ResponseEntity.ok("hello freom aslam");
+    }
+
 
 
 }
