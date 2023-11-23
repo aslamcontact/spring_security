cbf9122a515e7fa887e7a7d446f16c114508da7a
aslamcontact <aslam.mycontact@gmail.com>
2023-11-23 23:44:03 +0530

    add new security chain and constructor

--- src/main/java/com/aslam/mycontact/springtest/security/jwt/SecurityConfiguration.java
+++ src/main/java/com/aslam/mycontact/springtest/security/jwt/SecurityConfiguration.java
@@ -2,7 +2,11 @@ package com.aslam.mycontact.springtest.security.jwt;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,18 +18,24 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    @Autowired
-    private JwtAuthFilter jwtAuthFilter;
 
-    @Autowired
-    private AuthenticationProvider authenticationProvider;
+    private final JwtAuthFilter jwtAuthFilter;
 
+
+    private final AuthenticationProvider authenticationProvider;
+
+    public SecurityConfiguration(JwtAuthFilter jwtAuthFilter, AuthenticationProvider authenticationProvider) {
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 
         http.authorizeHttpRequests((auth)->
-                        auth.requestMatchers("").permitAll()
-                            .requestMatchers("").authenticated())
+                        auth.requestMatchers("/api/auth/**").permitAll()
+                            .requestMatchers("/api/user/**").authenticated())
                 .sessionManagement((session)->session
                                              .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
