d7372ed170decdd40e6c06ecb5cb53c11ef47d86
aslamcontact <aslam.mycontact@gmail.com>
2023-11-24 23:56:37 +0530

    disable csrf to use post method

--- src/main/java/com/aslam/mycontact/springtest/auth/AuthController.java
+++ src/main/java/com/aslam/mycontact/springtest/auth/AuthController.java
@@ -3,10 +3,7 @@ package com.aslam.mycontact.springtest.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/auth")
 @RestController
@@ -20,10 +17,14 @@ public class AuthController {
         this.authService = authService;
     }
 
+
+
     @PostMapping("/register")
     public ResponseEntity<AuthResponse>  registerUser(
                   @RequestBody RegisterRequest request)
     {
+
+        System.out.println(request.getEmail());
 
         return ResponseEntity.ok(authService.register(request));
 
--- src/main/java/com/aslam/mycontact/springtest/security/jwt/SecurityConfiguration.java
+++ src/main/java/com/aslam/mycontact/springtest/security/jwt/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -36,6 +37,7 @@ public class SecurityConfiguration {
         http.authorizeHttpRequests((auth)->
                         auth.requestMatchers("/api/auth/**").permitAll()
                             .requestMatchers("/api/user/**").authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement((session)->session
                                              .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
